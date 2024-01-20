1e9609402607415768af54a6af5f9c308ee21029
pennmaster2207 <2017yangd@gmail.com>
2024-01-19 22:18:20 -0800

    t

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 #TESTETSTESTETSTSTET
+print("test")
+import pygetwindow as gw
 
+def get_active_tab_url():
+    active_window = gw.getActiveWindow()
+    if active_window:
+        return active_window.title
+    else:
+        return None
+
+# Example usage
+current_tab_url = get_active_tab_url()
+print("hi")
+print("Current tab URL:", current_tab_url)
